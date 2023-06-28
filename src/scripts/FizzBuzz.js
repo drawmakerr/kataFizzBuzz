e305565c296167037cc25b7acc6e088922ae1056
Miguel <miguelpechero80@gmail.com>
2023-06-28 10:18:12 +0200

    testing final fizzbuzz

--- src/scripts/FizzBuzz.js
+++ src/scripts/FizzBuzz.js
@@ -1,22 +1,18 @@
-  function createArray() {
+export function FizzBuzz() {
   const numbers = [];
 
-for (let i = 0; i <= 100; i++) {
+  for (let i = 0; i < 100; i++) {
     numbers[i] = i + 1;
-    
+
     if (numbers[i] % 3 == 0 && numbers[i] % 5 == 0) {
-        numbers [i] = "FizzBuzz";
-        } else if (numbers[i] % 3 == 0) {
-        numbers[i] = "Fizz";
-        } else if (numbers [i] % 5 == 0) {
-        numbers [i] = "Buzz";
-        } else {
-        numbers [i];
-        }
-    // console.log(numbers[i]);
-    } 
-    console.log(numbers);
-    return numbers; 
+      numbers[i] = "FizzBuzz";
+    } else if (numbers[i] % 3 == 0) {
+      numbers[i] = "Fizz";
+    } else if (numbers[i] % 5 == 0) {
+      numbers[i] = "Buzz";
+    }
+  }
+  return numbers;
 }
 
-createArray()
+FizzBuzz();
--- tests/FizzBuzz.test.js
+++ tests/FizzBuzz.test.js
@@ -1,11 +1,26 @@
-import FizzBuzz from './../src/scripts/FizzBuzz'
-import numbers from './../src/scripts/FizzBuzz'
-import { createArray } from './../src/scripts/FizzBuzz'
+import { FizzBuzz } from './../src/scripts/FizzBuzz.js'
 
-describe('si el numero es divisible entre 3 y 5 devuelve FizzBuzz', () => {
-    test('devuelve FizzBuzz', () => {
-        let expected = 'FizzBuzz'
-        let actual = numbers
-        expect(actual).toBe(expected);
+
+  describe('FizzBuzz kata', () => {
+    test('should return "Fizz" for numbers divisible by 3', () => {
+      expect(FizzBuzz()[2]).toBe('Fizz')  
+      expect(FizzBuzz()[5]).toBe('Fizz')
     })
-})
+
+    test('should return "Buzz" for numbers divisible by 5', () => {
+      expect(FizzBuzz()[4]).toBe('Buzz')
+      expect(FizzBuzz()[9]).toBe('Buzz')
+      
+    })
+
+    test('should return "FizzBuzz" for numbers divisible by both 3 and 5', () => {
+      expect(FizzBuzz()[14]).toBe('FizzBuzz')
+      expect(FizzBuzz()[29]).toBe('FizzBuzz')
+      
+    })
+
+    test('should return the number itself for numbers not divisible by 3 or 5', () => {
+      expect(FizzBuzz()[0]).toBe(1)
+      expect(FizzBuzz()[6]).toBe(7)
+    })
+  })
